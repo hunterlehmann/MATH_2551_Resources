22c2ab55db83bf6f7d73bdda507f2d71c9f83856
Hunter Lehmann <51711323+hunterlehmann@users.noreply.github.com>
2024-10-18 19:35:42 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,13 +10,13 @@ var ptx_lunr_docs = [
   "body": "  My MATH 2551 Website   copyright  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-1-1-three-dim-space",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-1-1-three-dim-space.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Three Dimensional Space",
+  "body": " Three Dimensional Space  Much of our study this semester will be of functions that can either be graphed in three dimensional space or are defined on points in , so we begin by giving some basic vocabulary for .   Points in are specified as an ordered triple . We plot these points as shown in the diagram above, with each coordinate specifying how far to move from the origin in each of the three orthogonal directions defining this space. These are given by the coordinate axes : the -axis is the set of all points with coordinates both equal to , and similarly for the other two axes. An extremely important feature of this coordinate system is that it is right-handed , which means that if you align your right thumb with the direction of the -axis, then your fingers curl from the -axis towards the -axis. We will have opportunities throughout the course to choose one of two possible orientations for a mathematical object, and we will consistently make the choice that results in a right-handed system.  Besides the three coordinate axes, we will often work with the coordinate planes: . Points on the -plane, for example, all have the form for some real numbers and . These planes split into eight octants . The ordering of these octants varies by reference, but the first octant is consistently the octant where all three coordinates are non-negative: .  "
 },
 {
   "id": "backmatter-2",
